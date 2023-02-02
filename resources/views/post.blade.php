486d8cbd6a0b25a6dab40174d32eeeebdc249652
Giga Todadze <giga@Gigas-MacBook-Pro.local>
2023-02-02 18:32:59 +0400

    initial

---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="/app.css">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>FlickHouse</title>
+    </head>
+    <body>
+     <?= $post;  ?>
+        <a href="/">Go back</a>
+    </body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="/app.css">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>FlickHouse</title>
+    </head>
+    <body>
+        <article>
+            <a href="/posts/my-first-post">Flick House</a>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut beatae corporis deserunt dignissimos dolorem enim fuga fugiat, impedit provident? Ea eos eum fugiat laborum minus, omnis provident quos ut. </p>
+        </article>
+
+        <article>
+            <a href="/posts/my-second-post">Flick House 2</a>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut beatae corporis deserunt dignissimos dolorem enim fuga fugiat, impedit provident? Ea eos eum fugiat laborum minus, omnis provident quos ut. </p>
+        </article>
+
+        <article>
+            <a href="/posts/my-third-post">Flick House 3</a>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur consequatur corporis earum est fuga impedit libero, magnam maxime natus neque, non omnis quia quibusdam sequi, sit. Accusamus debitis magnam unde.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto deserunt dolorum explicabo, impedit incidunt iusto labore maxime nostrum nulla porro qui quis quo reprehenderit rerum sit tenetur unde voluptate voluptatum.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut beatae corporis deserunt dignissimos dolorem enim fuga fugiat, impedit provident? Ea eos eum fugiat laborum minus, omnis provident quos ut. </p>
+        </article>
+    </body>
+</html>
