1544fb874f9cfe5add52374228bc57dd598301f6
Giga Todadze <giga@Gigas-MacBook-Pro.local>
2023-03-20 16:41:26 +0400

    ep 28 factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,53 +17,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
 
-        $user = User::factory()->create();
+       $user = User::factory()->create(['name'=>'John Doe']);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
+        Post::factory(5)->create(
+            [
+                'user_id'=>$user->id
+            ]);
+   
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
-        ]);
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => '<p> Lorem ipsum dolar sit amet.</p>',
-            'body' => '<p> Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet</p>',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-            'body' => '<p>Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet</p>',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-            'body' => '<p>Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet</p>',
-        ]);
+        //$work = Category::create([
+        //    'name' => 'Work',
+        //    'slug' => 'work',
+        //]);
+        //
+        //$family = Category::create([
+        //    'name' => 'Family',
+        //    'slug' => 'family',
+        //]);
+        //
+        //$personal = Category::create([
+        //    'name' => 'Personal',
+        //    'slug' => 'personal',
+        //]);
+        //
+        //Post::create([
+        //    'user_id' => $user->id,
+        //    'category_id' => $family->id,
+        //    'title' => 'My family Post',
+        //    'slug' => 'my-family-post',
+        //    'excerpt' => '<p> Lorem ipsum dolar sit amet.</p>',
+        //    'body' => '<p> Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet</p>',
+        //]);
+        //
+        //Post::create([
+        //    'user_id' => $user->id,
+        //    'category_id' => $work->id,
+        //    'title' => 'My work Post',
+        //    'slug' => 'my-work-post',
+        //    'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+        //    'body' => '<p>Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet</p>',
+        //]);
+        //
+        //Post::create([
+        //    'user_id' => $user->id,
+        //    'category_id' => $personal->id,
+        //    'title' => 'My personal Post',
+        //    'slug' => 'my-personal-post',
+        //    'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+        //    'body' => '<p>Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet</p>',
+        //]);
 
     }
 }
