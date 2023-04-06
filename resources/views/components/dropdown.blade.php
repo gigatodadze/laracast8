cabc363c9ce6ac7442a3493e6f97317b0bb82aaf
Giga Todadze <giga@Gigas-MacBook-Pro.local>
2023-04-06 19:21:12 +0400

    ep 36

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -6,7 +6,7 @@
         {{$trigger}}
     </div>
 
-    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl">
+    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl w-full z-50 overflow-auto max-h-52 ">
         {{-- Dropdown Links--}}
         {{$slot}}
 
