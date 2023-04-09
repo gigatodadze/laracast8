429b2f229fb64ae6c5e7e801a7ac01d0fee293de
Giga Todadze <giga@Gigas-MacBook-Pro.local>
2023-04-09 15:13:22 +0400

    ep 38

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,21 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class  Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id','title','excerpt','body','category_id','slug'];
+    protected $fillable = ['user_id', 'title', 'excerpt', 'body', 'category_id', 'slug'];
 
-    protected $with =['category','author'];
+    protected $with = ['category', 'author'];
+
+    public function scopeFilter($query, array $filters) //Post::newQuery()->filter()
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) => $query
+            ->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%'));
+    }
 
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
 
-    public function author(){
-        return $this->belongsTo(User::class,'user_id');
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     //public function getRouteKeyName()
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
@@ -15,27 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $posts = Post::latest();
-    if (request('search')) {
-        $posts
-            ->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-    }
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-    return view('posts',
-        [
-            //'posts' => Post::latest()->with('category','author')->get(),
-            'posts' => $posts->get(),
-            'categories' => \App\Models\Category::all(),
-        ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post,
-    ]);
-});
+Route::get('posts/{post:slug}',[PostController::class,'show']);
 
 Route::get('categories/{category:slug}', function (\App\Models\Category $category) {
     return view('posts',
