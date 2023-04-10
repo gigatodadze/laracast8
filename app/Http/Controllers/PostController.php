302d76a42682bbae53e54bd74d67fa1d125c6d98
Giga Todadze <giga@Gigas-MacBook-Pro.local>
2023-04-10 14:20:14 +0400

    ep 39

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,8 +13,9 @@ class PostController extends Controller
         return view('posts',
             [
                 //'posts' => Post::latest()->with('category','author')->get(),
-                'posts' => Post::latest()->filter(request(['search']))->get(),
+                'posts' => Post::latest()->filter(request(['search','category']))->get(),
                 'categories' => Category::all(),
+                'currentCategory' => Category::firstWhere('slug',request('category'))
             ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,15 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}',[PostController::class,'show']);
 
-Route::get('categories/{category:slug}', function (\App\Models\Category $category) {
-    return view('posts',
-        [
-            'posts' => $category->posts,
-            'currentCategory' => $category,
-            'categories' => \App\Models\Category::all()
-            //'posts' => $category->posts->load('category','author'),
-        ]);
-})->name('category');
+//Route::get('categories/{category:slug}', function (\App\Models\Category $category) {
+//    return view('posts',
+//        [
+//            'posts' => $category->posts,
+//            'currentCategory' => $category,
+//            'categories' => \App\Models\Category::all()
+//            //'posts' => $category->posts->load('category','author'),
+//        ]);
+//})->name('category');
 
 Route::get('authors/{author:username}', function (\App\Models\User $author) {
     return view('posts',
